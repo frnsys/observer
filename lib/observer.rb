32077c6471a82833783460ec1ee35904e5a53e39
Francis Tseng <ftzeng@gmail.com>
2013-02-16 00:12:36 -0800

    working on streamlining

--- lib/observer.rb
+++ lib/observer.rb
@@ -58,8 +58,11 @@ module Observer
 		def self.spawn
 			# sd = script directory. Where this file resides.
 			sd = sanitizePath( File.expand_path(File.dirname(__FILE__)) )
+
 			# wd = working directory. Where the script has been called.
 			@wd = sanitizePath( File.expand_path(".") )
+
+			# Observer prototype and new "instance"
 			sdo = sd + "observer/observer.yml"
 			wdo = @wd + ".observer.yml"
 
@@ -77,18 +80,21 @@ module Observer
 		end
 
 		def syncFile( file, direction )
+			params = Hash.new
+
 			# Prepare filepaths
-			file = File.expand_path(file)
-			remote_filepath = file.sub( @od, @remote_path )
+			params[:file] = File.expand_path(file)
+			r_filepath = params[:file].sub( @od, @remote_path )
+
 			# A leading slash will mess things up.
 			# Get rid of it if it's there
-			remote_filepath = remote_filepath[0] == "/" ? remote_filepath[1..-1] : remote_filepath
-			remote_dirs = remote_filepath.split(File::SEPARATOR)
-			remote_file = remote_dirs.pop()
+			params[:r_filepath] = r_filepath[0] == "/" ? r_filepath[1..-1] : r_filepath
+			params[:r_dirs] = params[:r_filepath].split(File::SEPARATOR)
+			params[:r_file] = params[:r_dirs].pop()
 
 			if @debug == true
-				puts "file => #{file}"
-				puts "remote_filepath => #{remote_filepath}"
+				puts "file => #{params[:file]}"
+				puts "remote_filepath => #{params[:r_filepath]}"
 				puts "@od => #{@od}"
 				puts "@remote_path => #{@remote_path}"
 			end
@@ -115,7 +121,6 @@ module Observer
 
 					# Upload
 					if PUSH.include? direction
-						puts "Uploading #{remote_filepath}...".blue
 
 						# Since FTP puts us in the server's root dir,
 						# we need to look for the "public" or "www" dir:
@@ -130,38 +135,12 @@ module Observer
 							end
 						end
 
-						remote_dirs.each do |dir|
-							if !ftp.nlst().include?(dir)
-								puts "Making #{dir}"
-								ftp.mkdir(dir)
-							end
-							ftp.chdir(dir)
-						end
-
-						# Check for existing file
-						# and compare modification time
-						if ftp.nlst().include? remote_file
-							if ftp.mtime(remote_file).to_i > File.stat(file).mtime.to_i
-									puts "The file you are uploading is older than the one on the server. Do you want to overwrite the server file? (y/n)".red
-									if !yes?( STDIN.gets.chomp )
-										ftp.close()
-										return
-									end
-							end
-						end
-
-						ftp.putbinaryfile(file, remote_file)
-						puts "Upload successful.".green
+						upload( ftp, params )
 
 					# Download
 					elsif PULL.include? direction
-						begin
-							ftp.getbinaryfile(remote_filepath, file)
-						rescue Net::FTPPermError
-							# File doesn't exist
-							puts "#{remote_filepath} doesn't seem to exist!".red
-							return
-						end
+						download( ftp, params )
+
 					end
 
 					ftp.close()
@@ -169,127 +148,15 @@ module Observer
 
 			# SFTP
 			elsif @type.casecmp("sftp") == 0
-				Net::SSH.start( @server, @user, :password => @password ) do |sftp|
+				Net::SFTP.start( @server, @user, :password => @password ) do |sftp|
+
 					# Upload
 					if PUSH.include? direction
-						puts "Uploading #{remote_filepath}...".blue
-
-						remote_dirs.each do |dir|
-							dirs = sftp.dir.entries(".").map { |dir| dir.name }
-							if !dirs.include?(dir)
-								puts "Making #{dir}"
-								sftp.mkdir!(dir)
-							end
-							sftp.chdir(dir)
-
-							puts sftp.nlst()
-
-							exit
-						end
-
-						sftp.dir.foreach(".") do |dir|
-							puts dir.name
-						end
-						exit
-
-						# Check if the file already exists
-						begin
-							existing_file = sftp.file.open(remote_file)
-
-							# Warn user that they're overwriting a newer file
-							if File.stat(file).mtime.to_i < existing_file.stat.mtime
-								puts "The file you are uploading is older than the one on the server. Do you want to overwrite the server file? (y/n)".red
-								if !yes?( STDIN.gets.chomp )
-									existing_file.close()
-									return
-								end
-							end
-							existing_file.close()
-						rescue
-							# File doesn't exist
-						end
-
-						# Try the upload
-						begin
-							sftp.upload!( file, remote_file )
-							success = true
-
-						# If it doesn't work,
-						# there are probably missing subdirs
-						# net-sftp will throw this error:
-						rescue Net::SFTP::StatusException
-							# Keep track of the dir we need to create
-							dirs = Array.new
-							dirs << File.dirname(remote_file)
-
-							while true do
-								begin
-									# Check if each parent dir exists
-									parent_dir = dirs.last[0] == "/" ? dirs.last[1..-1] : dirs.last
-									puts "parent_dir => #{parent_dir}" if @debug == true
-									sftp.open!( parent_drr )
-
-								rescue Net::SFTP::StatusException
-									# If it doesn't exist, keep track
-									dirs << File.expand_path( File.join( dirs.last, ".." ) ).sub( @wd, "")
-									
-								else
-									# Found a dir that exists!
-									# The last dir will be the first dir that already exists.
-									# So we remove it.
-									dirs.pop
-
-									# Now create the directories
-									dirs.reverse_each do |dir|
-										puts "Making #{dir}"
-										sftp.mkdir!(dir)
-									end
-									break
-								end
-							end
-
-							# If it failed, try uploading the file again
-							sftp.upload!( file, remote_file ) if !success
-							puts "Upload successful.".green
-
-						end
+						upload( sftp, params )
 
 					# Download
 					elsif PULL.include? direction
-						
-						# Check if the file already exists
-						if File.exists?(file)
-							begin
-								existing_file = sftp.file.open(remote_file)
-
-								# Warn user that they're overwriting a newer file
-								if File.stat(file).mtime.to_i > existing_file.stat.mtime
-									puts "The file you are downloading is older than the local copy. Do you want to overwrite the local file? (y/n)".red
-									if !yes?( STDIN.gets.chomp )
-										existing_file.close()
-										return
-									end
-								end
-								existing_file.close()
-							rescue
-								# File doesn't exist
-								puts "#{remote_file} doesn't seem to exist!".red
-								return
-							end
-						end
-
-						# Try the download
-						begin
-							sftp.download!( remote_file, file )
-
-						rescue Net::SFTP::StatusException, RuntimeError
-							puts "#{remote_file} doesn't seem to exist!".red
-							return
-
-						else
-							puts "Downloaded #{remote_file}".blue
-
-						end
+						download( sftp, params )
 
 					end
 				end
@@ -316,13 +183,65 @@ module Observer
 
 			end
 		end
+
+		# Push
 		def push( item )
-			sync( item , "push" )
+			sync( item, "push" )
 		end
+		def upload( ftp, params )
+			r_filepath = params[:r_filepath]
+			r_dirs = params[:r_dirs]
+			r_file = params[:r_file]
+			file = params[:file]
+
+			puts "Uploading #{r_filepath}...".blue
+
+			r_dirs.each do |dir|
+				if !ftp.nlst().include?(dir)
+					puts "Making #{dir}"
+					ftp.mkdir(dir)
+				end
+				ftp.chdir(dir)
+			end
+
+			# Check for existing file
+			# and compare modification time
+			if ftp.nlst().include? r_file
+				if ftp.mtime(r_file).to_i > File.stat(file).mtime.to_i
+						puts "The file you are uploading is older than the one on the server. Do you want to overwrite the server file? (y/n)".red
+						if !yes?( STDIN.gets.chomp )
+							ftp.close()
+							return
+						end
+				end
+			end
+
+			ftp.putbinaryfile(file, r_file)
+			puts "Upload successful.".green
+		end
+
+		# Pull
 		def pull( item )
 			sync( item, "pull" )
 		end
+		def download( ftp, params )
+			r_filepath = params[:r_filepath]
+			r_dirs = params[:r_dirs]
+			r_file = params[:r_file]
+			file = params[:file]
 
+			puts "Downloading #{r_filepath}...".blue
+
+			begin
+				ftp.getbinaryfile(r_filepath, file)
+			rescue
+				# File doesn't exist
+				puts "#{r_filepath} doesn't seem to exist!".red
+				return
+			end
+		end
+
+		# Observe
 		def observe
 			puts "Observing #{@ward} for changes...".blue
 
@@ -349,19 +268,19 @@ module Observer
 
 		private
 		def collectFiles( folder )
-				files = Hash.new
-				Find.find( folder ) do |path|
-					if @ignore.include?( File.basename(path) )
-						Find.prune
-						
-					else
-						# We only want to include files, not folders
-						if !File.directory?(path)
-							files[path] = File.stat(path).mtime.to_i
-						end
+			files = Hash.new
+			Find.find( folder ) do |path|
+				if @ignore.include?( File.basename(path) )
+					Find.prune
+					
+				else
+					# We only want to include files, not folders
+					if !File.directory?(path)
+						files[path] = File.stat(path).mtime.to_i
 					end
 				end
-				return files
+			end
+			return files
 		end
 
 		# Most paths need to have a trailing "/"
--- lib/observer/sftp_compatibility.rb
+++ lib/observer/sftp_compatibility.rb
@@ -1,12 +1,14 @@
+# This mirrors the methods of Ruby's Net::FTP module.
 # From Philip Matarese
 # http://rubyforge.org/projects/sftp-compat/
+# Updated by Francis Tseng
 
 module Net
 	module SFTP
     class Session
       def mkdir(path)
         @current_path ||= '.'
-        method_missing :mkdir, File.join(@current_path, path), {}
+        mkdir!( File.join(@current_path, path) )
       end
 
       def rmdir(path)
@@ -27,15 +29,15 @@ module Net
         @current_path ||= '.'
 
         begin
-          handle = open_handle(File.join(@current_path, remotefile), "w")
+          handle = open(File.join(@current_path, remotefile), "w")
           file = Kernel.open(localfile)
           file.binmode
-          result = write(handle, file.read)
+          result = write(handle, 0, file.read)
 
-          raise(StandardError, result.message) unless result.code == 0
+          # raise(StandardError, result.message) unless result.code == 0
         ensure
           file.close
-          close_handle(handle)
+          close(handle)
         end
       end
 
@@ -46,11 +48,17 @@ module Net
 
       def nlst
         @current_path ||= '.'
-        handle = opendir(@current_path)
-        list = readdir(handle).map{|f| f.filename}
-        close_handle(handle)
+        #handle = opendir(@current_path)
+				handle = @current_path
+        list = dir.entries(handle).map{|f| f.name}
+        close(handle)
         return list
       end
+
+			def mtime(path)
+				@current_path ||= '.'
+				return stat!( File.join(@current_path, path) ).mtime
+			end
     end
   end
 end
